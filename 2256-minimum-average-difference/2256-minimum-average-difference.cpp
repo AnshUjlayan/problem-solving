81598175581d001815bc4d36158dd97ee856447c
AnshUjalayn <86222479+AnshUjalayn@users.noreply.github.com>
2022-12-04 23:04:49 +0530

    Time: 333 ms (32.13%), Space: 108.4 MB (5.11%) - LeetHub

---
+++ 2256-minimum-average-difference/2256-minimum-average-difference.cpp
@@ -0,0 +1,35 @@
+class Solution 
+{
+public:
+    int minimumAverageDifference(vector<int>& nums) 
+    {
+        int n = nums.size();
+        vector<long long> uno, dos;
+        long temp, sum = 0;
+        long ans = INT_MAX;
+        int ret = 0;
+        for(int i = 0; i < n; i++)
+        {
+            sum += nums[i];
+            uno.push_back(sum/(i+1));
+        }
+        for(int i = 0; i < n; i++)
+        {
+            sum -= nums[i];
+            if(n - i - 1 > 0)
+                dos.push_back(sum / (n - i - 1));
+            else
+                dos.push_back(0);
+        }
+        for(int i = 0; i < n; i++)
+        {
+            temp = abs(uno[i] - dos[i]);
+            if(temp < ans)
+            {
+                ans = temp;
+                ret = i;
+            }
+        }
+        return ret;
+    }
+};
