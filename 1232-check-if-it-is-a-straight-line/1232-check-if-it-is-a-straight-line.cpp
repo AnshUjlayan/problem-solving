55ea78104c72c429c7b06e50f494181badc1d74c
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2023-06-05 10:00:24 +0530

    Time: 3 ms (99.52%), Space: 10 MB (91.90%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
@@ -1,6 +1,9 @@
 class Solution {
 private:
     double getSlope(vector<vector<int>>& coordinates, int i, int j) {
+        if(coordinates[i][0] < coordinates[j][0]) {
+            swap(i, j);
+        }
         int x1 = coordinates[i][0];
         int y1 = coordinates[i][1];
         int x2 = coordinates[j][0];
@@ -9,7 +12,6 @@ private:
     }
 public:
     bool checkStraightLine(vector<vector<int>>& coordinates) {
-        sort(coordinates.begin(), coordinates.end());
         double slope = getSlope(coordinates, 0, 1);
         for(int i = 2; i < coordinates.size(); i++) {
             if(slope != getSlope(coordinates, 0, i)) {
