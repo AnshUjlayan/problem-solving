5438274303c5f7abc9d7a1086432e233f1577eeb
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2023-07-02 21:44:46 +0530

    Time: 7 ms (99.25%), Space: 9.3 MB (99.86%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,28 +1,32 @@
 class Solution {
+private:
+    int dfs(vector<vector<int>>& grid, int i, int j) {
+        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size()) {
+            return 0;
+        }
+        if(grid[i][j] == 1) {
+            return 1;
+        }
+        grid[i][j] = 1;
+        int a = dfs(grid, i + 1, j);
+        int b = dfs(grid, i - 1, j);
+        int c = dfs(grid, i, j + 1);
+        int d = dfs(grid, i, j - 1);
+        if(a && b && c && d) {
+            return 1;
+        }
+        return 0;
+    }
 public:
     int closedIsland(vector<vector<int>>& grid) {
-        int res = 0;
-        for (int i = 0; i < grid.size(); i++){
-            for (int j = 0; j < grid[0].size(); j++){
-                if (grid[i][j] == 0){
-                    res += dfs(grid, i, j) ? 1 : 0;
-                }
+        int result = 0, n = grid.size(), m = grid[0].size();
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == 0) {
+                    result += dfs(grid, i, j);
+                } 
             }
         }
-        return res;
-    }
-    bool dfs(vector<vector<int>>& g, int i, int j){
-        if (i < 0 || j < 0 || i >= g.size() || j >= g[0].size()){
-            return false;
-        }
-        if (g[i][j] == 1){
-            return true;
-        }
-        g[i][j] = 1;
-        bool d1 = dfs(g, i+1, j);
-        bool d2 = dfs(g, i, j+1);
-        bool d3 = dfs(g, i-1, j);
-        bool d4 = dfs(g, i, j-1);
-        return d1 && d2 && d3 && d4;
+        return result;
     }
 };
