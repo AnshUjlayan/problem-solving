3670d53469aacdcf61c04e9c7b86b669f13360eb
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2023-08-09 16:31:01 +0530

    Time: 152 ms (86.34%) | Memory: 81.1 MB (82.46%) - LeetSync

---
+++ 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private:
+    bool isValid(vector<int>& nums, int p, int num) {
+        for(int i = 0; i < nums.size() - 1; i++) {
+            if(nums[i + 1] - nums[i] <= num) {
+                p--;
+                i++;
+            }
+        }
+        return p <= 0;
+    }
+public:
+    int minimizeMax(vector<int>& nums, int p) {
+        sort(nums.begin(), nums.end());
+        int l = 0, r = INT_MAX;
+        int result = r;
+        while(l <= r) {
+            int mid = l + (r - l) / 2;
+            if(isValid(nums, p, mid)) {
+                result = mid;
+                r = mid - 1;
+            }
+            else {
+                l = mid + 1;
+            }
+        }
+        return result;
+    }
+};
