ae219c1818eb62207d706c02ceda3f9e47a01798
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2023-08-11 17:15:13 +0530

    Time: 4 ms (85.66%) | Memory: 8.9 MB (47.68%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,18 +1,15 @@
 class Solution {
-private:
-    int func(vector<vector<int>>& dp, vector<vector<int>>& triangle, int level, int idx) {
-        if(level == triangle.size()) {
-            return 0;
-        }
-        if(dp[level][idx] != -1) {
-            return dp[level][idx];
-        }
-        return dp[level][idx] = triangle[level][idx] + min(func(dp, triangle, level + 1, idx), func(dp, triangle, level + 1, idx + 1));
-    }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n, vector<int>(triangle[n - 1].size(), -1));
-        return func(dp, triangle, 0, 0);
+        vector<int> dp(triangle[n - 1].size() + 1, 0);
+        for(int i = n - 1; i >= 0; i--) {
+            vector<int> newDp(triangle[i].size());
+            for(int j = 0; j < triangle[i].size(); j++) {
+                newDp[j] = triangle[i][j] + min(dp[j], dp[j + 1]);
+            }
+            dp = newDp;
+        }
+        return dp[0];
     }
 };
