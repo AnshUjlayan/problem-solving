75a01bd55e0691991222998e01adb380004f5914
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2023-08-14 18:31:54 +0530

    Time: 2 ms (46.81%) | Memory: 5.9 MB (41.71%) - LeetSync

---
+++ 371-sum-of-two-integers/sum-of-two-integers.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        while(b != 0) {
+            int sum1 = a ^ b;
+            int sum2 = a & b;
+            sum2 <<= 1;
+            a = sum1;
+            b = sum2;
+        }
+        return a;
+    }
+};
