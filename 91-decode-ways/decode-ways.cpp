f9583b2953c738f8854f76727bccae38a867d7c7
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2023-08-15 19:03:08 +0530

    Time: 8 ms (9.21%) | Memory: 6.9 MB (26.69%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        vector<int> dp(s.size(), -1);
+        vector<int> dp(s.size() + 1, 0);
+        dp[s.size()] = 1;
         unordered_set<string> ust;
         for(int i = 1; i <= 26; i++) {
             ust.insert(to_string(i));
         }
-        function<int(int)> func = [&](int idx) {
-            if(idx == s.size()) {
-                return 1;
+        for(int i = s.size() - 1; i >= 0; i--) {
+            if(ust.find(s.substr(i, 1)) != ust.end()) {
+                dp[i] += dp[i + 1];
             }
-            if(dp[idx] != -1) {
-                return dp[idx];
+            if(i + 1 < s.size() && ust.find(s.substr(i, 2)) != ust.end()) {
+                dp[i] += dp[i + 2];
             }
-            int ans = 0;
-            if(ust.find(s.substr(idx, 1)) != ust.end()) {
-                ans += func(idx + 1);
-            }
-            if(idx + 1 < s.size() && ust.find(s.substr(idx, 2)) != ust.end()) {
-                ans += func(idx + 2);
-            }
-            return dp[idx] = ans;
-        };
-        return func(0);
+        }
+        return dp[0];
     }
 };
