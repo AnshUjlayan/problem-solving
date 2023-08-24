a9eb5d206dfdc3019f7c9fc48b6a5c0e4f2ed608
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2023-08-24 16:34:14 +0530

    Time: 25 ms (74.22%) | Memory: 23.2 MB (56.65%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -9,25 +9,19 @@
  */
 
 class Solution {
-private:
-    pair<TreeNode*,int> result = {nullptr, INT_MIN};
-    pair<bool,bool> func(int depth, TreeNode* root, TreeNode* p, TreeNode* q) {
-        pair<bool,bool> check;
-        if(!root) {
-            return check;
-        }
-        pair<bool,bool> l = func(depth + 1, root->left, p, q);
-        pair<bool,bool> r = func(depth + 1, root->right, p, q);
-        check.first = (root->val == p->val) || l.first || r.first;
-        check.second = (root->val == q->val) || l.second || r.second;
-        if(check.first && check.second && result.second < depth) {
-            result = {root, depth};
-        }
-        return check;
-    }
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        func(0, root, p, q);
-        return result.first;
+        while(root) {
+            if(root->val > p->val && root->val > q->val) {
+                root = root->left;
+            }
+            else if(root->val < p->val && root->val < q->val) {
+                root = root->right;
+            }
+            else {
+                return root;
+            }
+        }
+        return nullptr;
     }
 };
