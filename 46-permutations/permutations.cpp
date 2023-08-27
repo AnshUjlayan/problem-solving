a511d41320aa9d9355fab6713786480c7db6a431
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2023-08-28 02:21:17 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (61.02%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,23 +1,26 @@
 class Solution {
 private:
-    void func(vector<vector<int>>& result, vector<int>& ans, vector<int>& nums) {
-        if(ans.size() == nums.size()) {
-            result.push_back(ans);
+    void func(vector<vector<int>>& result, vector<int>& permutation, vector<bool>& picked, vector<int>& nums, int remaining) {
+        if(remaining == 0) {
+            result.push_back(permutation);
             return;
         }
-        for(int& num : nums) {
-            if(find(ans.begin(), ans.end(), num) == ans.end()) {
-                ans.push_back(num);
-                func(result, ans, nums);
-                ans.pop_back();
+        for(int i = 0; i < nums.size(); i++) {
+            if(!picked[i]) {
+                permutation.push_back(nums[i]);
+                picked[i] = true;
+                func(result, permutation, picked, nums, remaining - 1);
+                permutation.pop_back();
+                picked[i] = false;
             }
         }
     }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> result;
-        vector<int> ans;
-        func(result, ans, nums);
+        vector<int> permutation;
+        vector<bool> picked(nums.size(), false);
+        func(result, permutation, picked, nums, nums.size());
         return result;
     }
 };
