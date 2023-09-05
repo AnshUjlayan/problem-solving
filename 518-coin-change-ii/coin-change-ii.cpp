5c13a63a918f1128a79d6bb9c576c946d896247d
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2023-09-05 22:29:32 +0530

    Time: 5 ms (88.66%) | Memory: 18.1 MB (58.65%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        vector<int> oldDp(amount + 1, 0);
-        oldDp[0] = 1;
-        for(int i = coins.size() - 1; i >= 0; i--) {
-            vector<int> dp(amount + 1, 0);
-            dp[0] = 1;
-            for(int j = 1; j < amount + 1; j++) {
-                dp[j] = oldDp[j];
+        vector<int> dp(amount + 1, 0);
+        dp[0] = 1;
+        for(int i = 0; i < coins.size(); i++) {
+            vector<int> nextDp = dp;
+            for(int j = 0; j < amount + 1; j++) {
                 if(j - coins[i] >= 0) {
-                    dp[j] += dp[j - coins[i]];
+                    nextDp[j] += nextDp[j - coins[i]];
                 }
             }
-            oldDp = dp;
+            dp = nextDp;
         }
-        return oldDp[amount];
+        return dp[amount];
     }
 };
