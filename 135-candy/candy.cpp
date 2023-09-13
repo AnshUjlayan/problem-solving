a32d37330c3da0ef1a36bc789adc62636b05e19e
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2023-09-13 07:56:15 +0530

    Time: 25 ms (23.25%) | Memory: 17.8 MB (31.94%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+    int candy(vector<int>& ratings) {
+      int result = ratings.size();
+      vector<int> candies(ratings.size(), 1);
+      for(int i = 0; i < ratings.size(); i++) {
+        int neighbourL = i - 1 >= 0 ? ratings[i - 1] : 1e5;
+        int neighbourR = i + 1 < ratings.size() ? ratings[i + 1] : 1e5;
+        int prevState = candies[i];
+        if(neighbourL < ratings[i]) {
+          candies[i] = max(candies[i], candies[i - 1] + 1);
+        }
+        if(neighbourR < ratings[i]) {
+          candies[i] = max(candies[i], candies[i + 1] + 1);
+        }
+        result += candies[i] - prevState;
+      }
+      for(int i = ratings.size() - 1; i >= 0; i--) {
+        int neighbourL = i - 1 >= 0 ? ratings[i - 1] : 1e5;
+        int neighbourR = i + 1 < ratings.size() ? ratings[i + 1] : 1e5;
+        int prevState = candies[i];
+        if(neighbourL < ratings[i]) {
+          candies[i] = max(candies[i], candies[i - 1] + 1);
+        }
+        if(neighbourR < ratings[i]) {
+          candies[i] = max(candies[i], candies[i + 1] + 1);
+        }
+        result += candies[i] - prevState;
+      }
+      return result;
+    }
+};
