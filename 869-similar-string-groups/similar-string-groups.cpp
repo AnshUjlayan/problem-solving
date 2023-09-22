e375a80c50946d9de06085b49101d4bf6a83f278
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2023-09-23 02:40:50 +0530

    Time: 462 ms (9.76%) | Memory: 174.7 MB (12.79%) - LeetSync

---
+++ 869-similar-string-groups/similar-string-groups.cpp
@@ -0,0 +1,73 @@
+class DSU {
+  private:
+    int groups;
+    vector<int> parent, rank;
+  public:
+    DSU(int n) : parent(n), rank(n, 0) {
+      this->groups = n;
+      for(int i = 0; i < n; i++) {
+        parent[i] = i;
+      }
+    }
+    int getGroups() {
+      return groups;
+    }
+    int find(int u) {
+      if(parent[u] != u) {
+        return parent[u] = find(parent[u]);
+      }
+      return u;
+    }
+    void join(int u, int v) {
+      int pu = find(u), pv = find(v);
+      if(pu == pv) {
+        return;
+      }
+      groups--;
+      if(rank[pu] > rank[pv]) {
+        parent[pv] = pu;
+      }
+      else if(rank[pv] > rank[pu]) {
+        parent[pu] = pv;
+      }
+      else {
+        parent[pv] = pu;
+        rank[pu]++;
+      }
+    }
+};
+
+class Solution {
+  private:
+    bool slimy(string a, string b) {
+      if(a.size() != b.size()) {
+        return false;
+      }
+      vector<int> freq(26, 0);
+      int slime = 0;
+      for(int i = 0; i < a.size(); i++) {
+        slime += a[i] == b[i] ? 0 : 1;
+        freq[a[i] - 'a']++;
+        freq[b[i] - 'a']--;
+      }
+      for(int& f : freq) {
+        if(f != 0) {
+          return false;
+        }
+      }
+      return slime == 0 || slime == 2;
+    }
+  public:
+    int numSimilarGroups(vector<string>& strs) {
+      int n = strs.size();
+      DSU dsu(n);
+      for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n; j++) {
+          if(slimy(strs[i], strs[j])) {
+            dsu.join(i, j);
+          }
+        }
+      }
+      return dsu.getGroups();
+    }
+};
