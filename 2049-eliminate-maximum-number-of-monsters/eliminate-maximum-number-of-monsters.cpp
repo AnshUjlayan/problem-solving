722e69f373e10b3541682011717e991eef0de138
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2023-11-07 06:03:52 +0530

    Time: 143 ms (20.53%) | Memory: 89.1 MB (29.33%) - LeetSync

--- 2049-eliminate-maximum-number-of-monsters/eliminate-maximum-number-of-monsters.cpp
+++ 2049-eliminate-maximum-number-of-monsters/eliminate-maximum-number-of-monsters.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
         priority_queue<double, vector<double>, greater<double>> pq;
-        int result = 1;
+        int result = 0;
         for(int i = 0; i < dist.size(); i++) {
             double arrivalTime = 1.0 * dist[i] / speed[i];
             pq.push(arrivalTime);
         }
-        pq.pop();
         while(!pq.empty() && pq.top() > result) {
             pq.pop();
             result++;
