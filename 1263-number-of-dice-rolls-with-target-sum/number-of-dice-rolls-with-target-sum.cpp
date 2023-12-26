fb97f93064fb3b5384eb2b33f76833363d038d0a
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2023-12-26 08:54:16 +0530

    Time: 49 ms (16.61%) | Memory: 8.6 MB (76.28%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -1,27 +1,24 @@
 class Solution {
-private:
-    int mod = 1e9 + 7;
-    vector<vector<int>> dp;
-    int func(int n, int k, int target) {
-        if(!n) {
-            return target == 0;
-        }
-        if(target < 0) {
-            return 0;
-        }
-        if(dp[n][target] != -1) {
-            return dp[n][target];
-        }
-        long long result = 0;
-        for(int i = 1; i <= k; i++) {
-            result += func(n - 1, k, target - i);
-            result %= mod;
-        }
-        return dp[n][target] = (int)result;
-    }
 public:
     int numRollsToTarget(int n, int k, int target) {
-        this->dp.resize(n + 1, vector<int>(target + 1, -1));
-        return func(n, k, target);
+        int mod = 1e9 + 7;
+        vector<int> dp(target + 1, 0);
+        dp[target] = 1;
+        for(int i = 0; i < n; i++) {
+            int m = dp.size();
+            vector<int> nextDp(m - 1, 0);
+            for(int j = 0; j < m; j++) {
+                for(int x = 1; x <= k; x++) {
+                    if(j - x < 0) {
+                        continue;
+                    }
+                    long long calc = (long long)dp[j] + nextDp[j - x];
+                    calc %= mod;
+                    nextDp[j - x] = calc;
+                }
+            }
+            dp = nextDp;
+        }
+        return dp[0];
     }
 };
