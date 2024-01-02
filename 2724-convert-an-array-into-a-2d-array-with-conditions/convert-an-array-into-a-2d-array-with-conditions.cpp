baccfb29ca9296865af28402e001201c705ef64a
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2024-01-02 17:45:26 +0530

    Time: 32 ms (5.35%) | Memory: 30.1 MB (54.07%) - LeetSync

---
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> findMatrix(vector<int>& nums) {
+        vector<vector<int>> result;
+        map<int,int> mp;
+        int rows = 0;
+        for(int& num : nums) {
+            rows = max(rows, ++mp[num]);
+        }
+        result.resize(rows);
+        for(auto& [key, val] : mp) {
+            for(int i = 0; i < val; i++) {
+                result[i].push_back(key);
+            }
+        }
+        return result;
+    }
+};
