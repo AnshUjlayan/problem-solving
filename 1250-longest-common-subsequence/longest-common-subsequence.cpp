5d3f3f54ca16bfeb75987be1fb13f19304599bc5
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2024-01-26 00:42:00 +0530

    Time: 47 ms (61.26%) | Memory: 25.9 MB (5.18%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,18 +1,21 @@
 class Solution {
-public:
-    int longestCommonSubsequence(string text1, string text2) {
-        int m = text1.size(), n = text2.size();
-        vector<int> dp(n + 1, 0);
-        for(int i = m - 1; i >= 0; i--) {
-            vector<int> nextDp = dp;
-            for(int j = n - 1; j >= 0; j--) {
-                nextDp[j] = max(nextDp[j + 1], dp[j]);
-                if(text1[i] == text2[j]) {
-                    nextDp[j] = 1 + dp[j + 1];
-                }
-            }
-            dp = nextDp;
+private:
+    vector<vector<int>> dp;
+    int getSlimy(string& text1, string& text2, int i, int j) {
+        if(i == text1.size() || j == text2.size()) {
+            return 0;
         }
-        return dp[0];
+        if(dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        if(text1[i] == text2[j]) {
+            return dp[i][j] = 1 + getSlimy(text1, text2, i + 1, j + 1);
+        }
+        return dp[i][j] = max(getSlimy(text1, text2, i + 1, j), getSlimy(text1, text2, i, j + 1));
+    }
+public:
+    int longestCommonSubsequence(string& text1, string& text2) {
+        this->dp.resize(text1.size(), vector<int>(text2.size(), -1));
+        return getSlimy(text1, text2, 0, 0);
     }
 };
