52221da5be0d21a6017b5af7165da637d6a6d46d
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2024-02-10 17:25:45 +0530

    Time: 3 ms (83.48%) | Memory: 7.4 MB (74.15%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int countSubstrings(string s) {
-        int result = 0;
-        for(int i = 0; i < s.size(); i++) {
-            int l = i, r = i;
-            while(l >= 0 && r < s.size() && s[l] == s[r]) {
-                result++;
-                l--;
-                r++;
+        int result = 0, n = s.size();
+        function<void(int, int)> getSlimy = [&] (int l, int r) {
+            while(l >= 0 && r < n) {
+                if(s[l] != s[r]) {
+                    return;
+                }
+                result++, l--, r++;
             }
-            l = i; r = i + 1;
-            while(l >= 0 && r < s.size() && s[l] == s[r]) {
-                result++;
-                l--;
-                r++;
-            }
+        };
+        for(int i = 0; i < n; i++) {
+            getSlimy(i, i);
+            getSlimy(i, i + 1);
         }
         return result;
     }
