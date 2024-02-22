84f5bd062d06225906e3e1b25432041f9614bb1d
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2024-02-22 21:21:23 +0530

    Time: 121 ms (84.95%) | Memory: 64.3 MB (62.99%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int> nums1(n, 0), nums2(n, 0);
+        vector<int> nums(n, 0);
         for(auto& t : trust) {
-            nums1[t[1] - 1]++;
-            nums2[t[0] - 1]++;
+            nums[t[0] - 1]--;
+            nums[t[1] - 1]++;
         }
-        auto it1 = find(nums1.begin(), nums1.end(), n - 1) - nums1.begin() + 1;
-        auto it2 = find(nums2.begin(), nums2.end(), 0) - nums2.begin() + 1;
-        return it1 == it2 && it1 <= n ? it1 : -1;
+        auto it = find(nums.begin(), nums.end(), n - 1);
+        return (it != nums.end()) ? (it - nums.begin() + 1) : -1;
     }
 };
