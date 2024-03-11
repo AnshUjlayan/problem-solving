017ee854da6f728eda4ccbf18c5c212f391d09a0
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2024-03-11 23:45:42 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (66.00%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.cpp
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -1,21 +1,9 @@
 class Solution {
 public:
     string customSortString(string order, string s) {
-        vector<int> freq(26, 0);
-        string result = "";
-        for(char& c : s) {
-            freq[c - 'a']++;
-        }
-        for(char& c : order) {
-            while(freq[c - 'a']--) {
-                result += c;
-            }
-        }
-        for(int i = 0; i < 26; i++) {
-            while(freq[i]-- > 0) {
-                result += i + 'a';
-            }
-        }
-        return result;
+        sort(s.begin(), s.end(), [&order](char a, char b) {
+            return order.find(a) < order.find(b);
+        });
+        return s;
     }
 };
