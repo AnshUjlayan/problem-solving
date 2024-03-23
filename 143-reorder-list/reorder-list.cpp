2c9ddc3a93456340db3f7b20c00857a04847d7da
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2024-03-23 20:32:48 +0530

    Time: 24 ms (59.39%) | Memory: 23.7 MB (5.07%) - LeetSync

--- 143-reorder-list/reorder-list.cpp
+++ 143-reorder-list/reorder-list.cpp
@@ -1,53 +1,20 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
-private:
-    ListNode* getMiddle(ListNode* head) {
-        ListNode* slow = head;
-        ListNode* fast = head->next;
-        while(fast && fast->next) {
-            fast = fast->next->next;
-            slow = slow->next;
-        }
-        return slow;
-    }
-
-    ListNode* reverse(ListNode* head) {
-        ListNode* prev = nullptr;
-        while(head) {
-            ListNode* temp = head->next;
-            head->next = prev;
-            prev = head;
-            head = temp;
-        }
-        return prev;
-    }
-    void mergeLists(ListNode* head, ListNode* end) {
-        while(end) {
-            ListNode* tempStart = head->next;
-            ListNode* tempEnd = end->next;
-            head->next = end;
-            end->next = tempStart;
-            head = tempStart;
-            end = tempEnd;
-        }
-    }
 public:
-    void reorderList(ListNode* head) {
-        if(!head->next) {
-            return;
+    ListNode* reorderList(ListNode* head) {
+        vector<ListNode*> nodes, result;
+        while(head) {
+            nodes.push_back(head);
+            head = head->next;
         }
-        ListNode* middle = getMiddle(head);
-        ListNode* end = reverse(middle->next);
-        middle->next = nullptr;
-        mergeLists(head, end);
+        int n = nodes.size(), start = 0, end = nodes.size() - 1;
+        while(start <= end) {
+            result.push_back(nodes[start++]);
+            result.push_back(nodes[end--]);
+        }
+        for(int i = 0; i < n - 1; i++) {
+            result[i]->next = result[i + 1];
+        }
+        result[n - 1]->next = nullptr;
+        return result[0];
     }
 };
