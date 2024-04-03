bfb3a85e3bdcb62e6b8e1a31af5e5ab923647552
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2024-04-03 21:14:13 +0530

    Time: 543 ms (58.77%) | Memory: 10.7 MB (65.03%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,35 +1,32 @@
 class Solution {
 private:
     int m, n;
-    vector<vector<char>> board;
     vector<vector<bool>> visited;
-
-    bool dfs(string word, int i, int j, int w) {
+    bool dfs(vector<vector<char>>& board, string& word, int i, int j, int w) {
         if (w == word.size()) {
             return true;
         }
         if (i < 0 || j < 0 || i >= this->m || j >= this->n || visited[i][j] ||
-            this->board[i][j] != word[w]) {
+            board[i][j] != word[w]) {
             return false;
         }
         visited[i][j] = true;
-        bool result = dfs(word, i + 1, j, w + 1) ||
-                      dfs(word, i - 1, j, w + 1) ||
-                      dfs(word, i, j + 1, w + 1) || 
-                      dfs(word, i, j - 1, w + 1);
+        bool result = dfs(board, word, i + 1, j, w + 1) ||
+                      dfs(board, word, i - 1, j, w + 1) ||
+                      dfs(board, word, i, j + 1, w + 1) || 
+                      dfs(board, word, i, j - 1, w + 1);
         visited[i][j] = false;
         return result;
     }
 
 public:
-    bool exist(vector<vector<char>>& board, string word) {
-        this->board = board;
+    bool exist(vector<vector<char>>& board, string& word) {
         this->m = board.size();
         this->n = board[0].size();
         this->visited.resize(this->m, vector<bool>(this->n, false));
         for (int i = 0; i < this->m; i++) {
             for (int j = 0; j < this->n; j++) {
-                if (dfs(word, i, j, 0)) {
+                if (dfs(board, word, i, j, 0)) {
                     return true;
                 }
             }
