e9efa7cf0422d4804f574f986a5c1a8c1567c1b5
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2024-04-03 21:16:39 +0530

    Time: 581 ms (56.31%) | Memory: 10.7 MB (65.03%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,32 +1,31 @@
 class Solution {
 private:
-    int m, n;
-    vector<vector<bool>> visited;
-    bool dfs(vector<vector<char>>& board, string& word, int i, int j, int w) {
+    bool dfs(vector<vector<char>>& board, vector<vector<bool>>& visited,
+             string& word, int i, int j, int w) {
         if (w == word.size()) {
             return true;
         }
-        if (i < 0 || j < 0 || i >= this->m || j >= this->n || visited[i][j] ||
-            board[i][j] != word[w]) {
+        if (i < 0 || j < 0 || i >= board.size() || j >= board[0].size() ||
+            visited[i][j] || board[i][j] != word[w]) {
             return false;
         }
         visited[i][j] = true;
-        bool result = dfs(board, word, i + 1, j, w + 1) ||
-                      dfs(board, word, i - 1, j, w + 1) ||
-                      dfs(board, word, i, j + 1, w + 1) || 
-                      dfs(board, word, i, j - 1, w + 1);
+        bool result = dfs(board, visited, word, i + 1, j, w + 1) ||
+                      dfs(board, visited, word, i - 1, j, w + 1) ||
+                      dfs(board, visited, word, i, j + 1, w + 1) ||
+                      dfs(board, visited, word, i, j - 1, w + 1);
         visited[i][j] = false;
         return result;
     }
 
 public:
     bool exist(vector<vector<char>>& board, string& word) {
-        this->m = board.size();
-        this->n = board[0].size();
-        this->visited.resize(this->m, vector<bool>(this->n, false));
-        for (int i = 0; i < this->m; i++) {
-            for (int j = 0; j < this->n; j++) {
-                if (dfs(board, word, i, j, 0)) {
+        int m = board.size();
+        int n = board[0].size();
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dfs(board, visited, word, i, j, 0)) {
                     return true;
                 }
             }
