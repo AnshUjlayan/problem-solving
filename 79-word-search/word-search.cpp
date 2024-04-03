4f3d6d8188fb1336a4894a448da3224f2d2f35a1
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2024-04-03 21:10:57 +0530

    Time: 1216 ms (10.48%) | Memory: 12 MB (9.82%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,43 +1,37 @@
 class Solution {
 private:
-    vector<vector<int>> directions {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    bool boundsCheck(vector<vector<char>>& board, int x, int y) {
-        int m = board.size(), n = board[0].size();
-        return x >= 0 && y >= 0 && x < m && y < n;
-    }
+    int m, n;
+    vector<vector<char>> board;
+    vector<vector<bool>> visited;
 
-    bool dfs(vector<vector<char>>& board, vector<vector<bool>>& visited, string& word, int idx, int i, int j) {
-        if(board[i][j] == word[idx]) {
-            idx++;
-            if(idx == word.size()) {
-                return true;
-            }
+    bool dfs(string word, int i, int j, int w) {
+        if (w == word.size()) {
+            return true;
         }
-        else {
+        if (i < 0 || j < 0 || i >= this->m || j >= this->n || visited[i][j] ||
+            this->board[i][j] != word[w]) {
             return false;
         }
-        bool result = false;
-        for(auto& direction : directions) {
-            int x = i + direction[0], y = j + direction[1];
-            if(boundsCheck(board, x, y) && !visited[x][y]) {
-                visited[x][y] = true;
-                result = result || dfs(board, visited, word, idx, x, y);
-                visited[x][y] = false;
-            }
-        }
+        visited[i][j] = true;
+        bool result = dfs(word, i + 1, j, w + 1) ||
+                      dfs(word, i - 1, j, w + 1) ||
+                      dfs(word, i, j + 1, w + 1) || 
+                      dfs(word, i, j - 1, w + 1);
+        visited[i][j] = false;
         return result;
     }
+
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        int m = board.size(), n = board[0].size();
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                visited[i][j] = true;
-                if(dfs(board, visited, word, 0, i, j)) {
+        this->board = board;
+        this->m = board.size();
+        this->n = board[0].size();
+        this->visited.resize(this->m, vector<bool>(this->n, false));
+        for (int i = 0; i < this->m; i++) {
+            for (int j = 0; j < this->n; j++) {
+                if (dfs(word, i, j, 0)) {
                     return true;
                 }
-                visited[i][j] = false;
             }
         }
         return false;
