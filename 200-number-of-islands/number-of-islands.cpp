820c1036851ca3f846177a0f01826439f7f3f501
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2024-04-19 20:39:24 +0530

    Time: 19 ms (98.04%) | Memory: 15.7 MB (81.63%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -16,6 +16,8 @@ private:
 
 public:
     int numIslands(vector<vector<char>>& grid) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
         int result = 0, m = grid.size(), n = grid[0].size();
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
