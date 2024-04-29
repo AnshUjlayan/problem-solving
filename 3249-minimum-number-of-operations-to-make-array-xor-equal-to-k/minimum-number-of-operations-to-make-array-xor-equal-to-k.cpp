d2f9f5b321214a3ee1815f8fa245b12766b05a5c
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2024-04-29 20:32:46 +0530

    Time: 57 ms (98.78%) | Memory: 91.8 MB (8.20%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        int result = 0, res = 0;
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        int res = 0;
         for(int& num : nums) {
             res ^= num;
         }
-        while(res || k) {
-            result += (res & 1) ^ (k & 1);
-            res >>= 1;
-            k >>= 1;
-        }
-        return result;
+        res ^= k;
+        return __builtin_popcount(res);
     }
 };
