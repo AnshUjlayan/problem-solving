3107a72f60ea95eeb9e8f2714405184c604dfa5e
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2024-05-11 23:45:05 +0530

    Time: 216 ms (70.00%) | Memory: 12.3 MB (81.73%) - LeetSync

---
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+    double frac(int a, int b) {
+        return -static_cast<double>(a) / b;
+    }
+
+public:
+    vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+        priority_queue<pair<double,pair<int,int>>> pq;
+        int n = arr.size();
+        for(int i = 0; i < n - 1; i++) {
+            pq.push({frac(arr[i], arr[n - 1]), {i, n - 1}});
+        }
+        while(--k) {
+            auto [l, r] = pq.top().second;
+            pq.pop();
+            if(l < r) {
+                pq.push({frac(arr[l], arr[r - 1]), {l, r - 1}});
+            }
+        }
+        auto [l, r] = pq.top().second;
+        return vector<int>({arr[l], arr[r]});
+    }
+};
