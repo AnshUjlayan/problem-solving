7ca9a0ab56efef94b8158858d8c1563e2ffad6cf
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2024-05-16 00:06:11 +0530

    Time: 293 ms (98.40%) | Memory: 117.5 MB (96.49%) - LeetSync

---
+++ 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int maximumSafenessFactor(vector<vector<int>>& grid) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        vector<pair<int, int>> dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        vector<pair<int, int>> escapeDirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        queue<pair<int, int>> thief;
+        int m = grid.size(), n = grid[0].size();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j]) {
+                    thief.push({i, j});
+                }
+            }
+        }
+        while (!thief.empty()) {
+            auto [i, j] = thief.front();
+            thief.pop();
+            for (auto& [dirX, dirY] : dirs) {
+                int x = i + dirX, y = j + dirY;
+                if (x >= 0 && y >= 0 && x < m && y < n && !grid[x][y]) {
+                    thief.push({x, y});
+                    grid[x][y] = grid[i][j] + 1;
+                }
+            }
+        }
+        priority_queue<pair<int, pair<int, int>>> pq;
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        pq.push({grid[0][0], {0, 0}});
+        visited[0][0] = true;
+        while (!pq.empty()) {
+            auto safenessFactor = pq.top().first;
+            auto [i, j] = pq.top().second;
+            pq.pop();
+            if (i == m - 1 && j == n - 1) {
+                return safenessFactor - 1;
+            }
+            for (auto& [dirX, dirY] : escapeDirs) {
+                int x = i + dirX, y = j + dirY;
+                if (x >= 0 && y >= 0 && x < m && y < n && !visited[x][y]) {
+                    pq.push({min(safenessFactor, grid[x][y]), {x, y}});
+                    visited[x][y] = true;
+                }
+            }
+        }
+        return -1;
+    }
+};
