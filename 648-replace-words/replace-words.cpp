591e8347ed5c13c825980a2aa6ba00ecc24a294b
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2024-06-07 22:05:29 +0530

    Time: 492 ms (25.23%) | Memory: 329.2 MB (20.81%) - LeetSync

---
+++ 648-replace-words/replace-words.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string replaceWords(vector<string>& dictionary, string sentence) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        auto idx = sentence.begin();
+        auto comp = [] (string& x, string& y) -> bool { return x.size() < y.size(); };
+        sort(dictionary.begin(), dictionary.end(), comp);
+        while (idx < sentence.end()) {
+            for (string& word : dictionary) {
+                if (distance(idx, sentence.end()) >= word.size() && string(idx, idx + word.size()) == word) {
+                    sentence.replace(idx, find(idx, sentence.end(), ' '), word.begin(), word.end());
+                    break;
+                }
+            }
+            idx = find(idx, sentence.end(), ' ') + 1;
+        }
+        return sentence;
+    }
+};
