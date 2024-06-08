4b947574efc738ff70db9dc3a1f63b6aa753a779
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2024-06-08 19:20:29 +0530

    Time: 127 ms (98.12%) | Memory: 134.8 MB (31.96%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -3,7 +3,7 @@ public:
     bool checkSubarraySum(vector<int>& nums, int k) {
         ios_base::sync_with_stdio(false);
         cin.tie(nullptr);
-        map<int, int> mp{{0, -1}};
+        unordered_map<int, int> mp{{0, -1}};
         int sum = 0, n = nums.size();
         for (int i = 0; i < n; i++) {
             sum = (sum + nums[i]) % k;
