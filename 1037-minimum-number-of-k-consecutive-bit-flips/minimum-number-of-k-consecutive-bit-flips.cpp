079f731a035e7c728ce35213caaeb90cb1fb8a93
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2024-06-24 21:46:27 +0530

    Time: 69 ms (98.77%) | Memory: 110.6 MB (51.23%) - LeetSync

--- 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.cpp
+++ 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.cpp
@@ -4,16 +4,16 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(nullptr);
         int n = nums.size(), result = 0;
-        priority_queue<int, vector<int>, greater<int>> pq;
+        deque<int> dq;
         for (int i = 0; i < n; i++) {
-            while (!pq.empty() && pq.top() <= i)
-                pq.pop();
-            int cnt = pq.size() + nums[i];
+            while (!dq.empty() && dq.back() <= i)
+                dq.pop_back();
+            int cnt = dq.size() + nums[i];
             if (!(cnt & 1)) {
                 if (i + k > n)
                     return -1;
                 result++;
-                pq.push(i + k);
+                dq.push_front(i + k);
             }
         }
         return result;
