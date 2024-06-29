709c22b86ff2c4804942652cd954044bc31906cd
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2024-06-29 20:54:04 +0530

    Time: 84 ms (97.69%) | Memory: 63.3 MB (90.34%) - LeetSync

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,37 +1,24 @@
 class Solution {
+private:
+    void dfs(vector<vector<int>>& result, vector<vector<int>>& adj, int ancestor, int node) {
+        for(int& child : adj[node]) {
+            if(!result[child].size() || (result[child].back() != ancestor)) {
+                result[child].push_back(ancestor);
+                dfs(result, adj, ancestor, child);
+            }
+        }
+    }
+
 public:
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
         ios_base::sync_with_stdio(false);
         cin.tie(nullptr);
-
-        vector<vector<int>> result(n);
-        vector<set<int>> ancestors(n);
-        vector<vector<int>> adj(n);
-        vector<int> degree(n, 0);
-        queue<int> q;
-        
+        vector<vector<int>> result(n), adj(n);
         for(auto& edge : edges) {
             adj[edge[0]].push_back(edge[1]);
-            degree[edge[1]]++;
         }
-
         for(int i = 0; i < n; i++) {
-            if (degree[i] == 0) {
-                q.push(i);
-            }
-        }
-        
-        while(!q.empty()) {
-            int node = q.front();
-            q.pop();
-            result[node] = vector<int>(ancestors[node].begin(), ancestors[node].end());
-            for (int& child : adj[node]) {
-                ancestors[child].insert(node);
-                ancestors[child].insert(ancestors[node].begin(), ancestors[node].end());
-                if (--degree[child] == 0) {
-                    q.push(child);
-                }
-            }
+            dfs(result, adj, i, i);
         }
         return result;
     }
