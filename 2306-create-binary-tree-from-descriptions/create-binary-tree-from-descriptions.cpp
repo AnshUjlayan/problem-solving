81474bf802a55e7d14240c111f943cdae4c5ea8c
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2024-07-15 14:56:39 +0530

    Time: 703 ms (56.55%) | Memory: 276.3 MB (59.39%) - LeetSync

---
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    unordered_map<int,TreeNode*> ump;
+    TreeNode* getNode(int n) {
+        if (ump.count(n)) {
+            return ump[n];
+        }
+        return ump[n] = new TreeNode(n);
+    }
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        unordered_map<int,int> degree;
+        for (auto& desc : descriptions) {
+            degree[desc[0]] = degree[desc[0]];
+            degree[desc[1]]++;
+            if (desc[2]) {
+                getNode(desc[0])->left = getNode(desc[1]);
+            }
+            else {
+                getNode(desc[0])->right = getNode(desc[1]);
+            }
+        }
+        for (auto& [key, val] : degree) {
+            if (!val) {
+                return getNode(key);
+            }
+        }
+        return nullptr;
+    }
+};
