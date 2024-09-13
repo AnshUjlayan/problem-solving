4476c16bdc58266593225eb44e3ac9d619b79bfd
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2024-09-13 11:39:39 +0530

    Time: 34 ms (98.74%) | Memory: 42.7 MB (32.98%) - LeetSync

---
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        int n = arr.size(), tot = 0;
+        vector<int> result, left {0};
+        for (int i = 0; i < n; i++) {
+            tot ^= arr[i];
+            left.push_back(tot);
+        }
+        for (vector<int> &query: queries) {
+            result.push_back(left[query[0]] ^ left[query[1] + 1]);
+        }
+        return result;
+    }
+};
