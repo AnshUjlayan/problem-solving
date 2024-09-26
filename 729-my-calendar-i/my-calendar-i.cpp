ac162818ac031eb8776e109c12dba04ca95cf48f
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2024-09-26 06:24:46 +0530

    Time: 64 ms (95.05%) | Memory: 42.8 MB (36.63%) - LeetSync

---
+++ 729-my-calendar-i/my-calendar-i.cpp
@@ -0,0 +1,19 @@
+class MyCalendar {
+public:
+    set<pair<int, int>> calendar;
+    bool book(int start, int end) {
+        const pair<int, int> event{start, end};
+        const auto nextEvent = calendar.lower_bound(event);
+        if (nextEvent != calendar.end() && nextEvent->first < end) {
+            return false;
+        }
+        if (nextEvent != calendar.begin()) {
+            const auto prevEvent = prev(nextEvent);
+            if (prevEvent->second > start) {
+                return false;
+            }
+        }
+        calendar.insert(event);
+        return true;
+    }
+};
