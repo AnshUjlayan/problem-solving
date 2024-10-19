528c33540a548bca3d41921190d39d82b5ae71eb
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2024-10-19 12:53:12 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (64.16%) - LeetSync

---
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    char findKthBit(int n, int k) {
+        int positionInSection = k & -k;
+        bool isInInvertedPart = ((k / positionInSection) >> 1 & 1) == 1;
+        bool originalBitIsOne = (k & 1) == 0;
+        if (isInInvertedPart) {
+            return originalBitIsOne ? '0' : '1';
+        }
+        return originalBitIsOne ? '1' : '0';
+    }
+};
