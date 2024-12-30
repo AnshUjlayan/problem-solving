4ee7177bb92b6ab06df7434b9722609e1f9e78c7
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2024-12-30 19:08:59 +0530

    Time: 0 ms (100.00%) | Memory: 45.9 MB (39.53%) - LeetSync

--- 1063-best-sightseeing-pair/best-sightseeing-pair.cpp
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.cpp
@@ -3,12 +3,10 @@ public:
     int maxScoreSightseeingPair(vector<int>& values) {
         ios_base::sync_with_stdio(false);
         cin.tie(nullptr);
-        int result = 0, x = 0;
-        priority_queue<int> pq;
-        pq.push(0);
+        int result = 0, x = 0, mx = 0;
         for(int &val : values) {
-            result = max(result, pq.top() - x + val);
-            pq.push(val + x++);
+            result = max(result, mx - x + val);
+            mx = max(mx, val + x++);
         }
         return result;
     }
