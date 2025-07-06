c2fdf27c8abd68346a3e8b468567a7cbb80d61d4
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2025-07-07 00:16:49 +0530

    Time: 335 ms (73.14%) | Memory: 78.7 MB (88.08%) - LeetSync

--- 3618-find-the-original-typed-string-ii/find-the-original-typed-string-ii.cpp
+++ 3618-find-the-original-typed-string-ii/find-the-original-typed-string-ii.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int possibleStringCount(string word, int k) {
+        // because im a sweat
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+
         const int MOD = 1e9 + 7;
         long long res = 1, cnt = 1;
 
@@ -18,22 +22,21 @@ public:
         res = (res * cnt) % MOD;
 
         // initialise dp vectors
-        vector<int>* dp = new vector<int>(k, 0);
-        vector<int>* nextDp = new vector<int>(k, 0);
-        fill(dp->begin() + max(0, k - arr[0] - 1), dp->end(), 1);
+        vector<int> dp(k, 0), nextDp(k, 0);
+        fill(dp.begin() + max(0, k - arr[0] - 1), dp.end(), 1);
         k -= 2;
 
         // main logic
         for (int i = 1; i < arr.size(); i++) {
             if (k < 0) break;
             for (int j = k; j >= 0; j--) {
-                (*dp)[j] = ((*dp)[j] + (*dp)[j + 1]) % MOD;
-                (*nextDp)[j] = (0ll + (*dp)[j + 1] - (*dp)[min(j + arr[i] + 1, k + 1)]) % MOD;
+                dp[j] = (dp[j] + dp[j + 1]) % MOD;
+                nextDp[j] = (0ll + dp[j + 1] - dp[min(j + arr[i] + 1, k + 1)] + MOD) % MOD;
             }
             swap(dp, nextDp);
-            fill(nextDp->begin(), nextDp->begin() + max(0, k--), 0);
+            fill(nextDp.begin(), nextDp.begin() + max(0, k--), 0);
         }
 
-        return ((res - accumulate(dp->begin(), dp->begin() + max(0, k + 1), 0ll) % MOD) + MOD) % MOD;
+        return ((res - accumulate(dp.begin(), dp.begin() + max(0, k + 1), 0ll) % MOD) + MOD) % MOD;
     }
 };
