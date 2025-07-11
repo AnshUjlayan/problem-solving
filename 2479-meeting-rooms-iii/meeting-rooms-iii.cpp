0897049be9848f61ac6c057d05d10d5a8da398bf
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2025-07-12 00:04:25 +0530

    Time: 106 ms (42.46%) | Memory: 114.9 MB (15.36%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
@@ -1,42 +1,54 @@
 class Solution {
+private:
+    struct Meeting {
+        long long start, end;
+        int roomId;
+        Meeting(long long start, long long end, int roomId) : start(start), end(end), roomId(roomId) {}
+    };
+
 public:
+    Solution() { ios_base::sync_with_stdio(false); cin.tie(nullptr); }
+
     int mostBooked(int n, vector<vector<int>>& meetings) {
-        vector<long long> roomAvailabilityTime(n, 0);
-        vector<int> meetingCount(n, 0);
         sort(meetings.begin(), meetings.end());
 
-        for (auto& meeting: meetings) {
-            int start = meeting[0], end = meeting[1];
-            long long minRoomAvailabilityTime = LLONG_MAX;
-            int minAvailableTimeRoom = 0;
-            bool foundUnusedRoom = false;
-
-            for (int i = 0; i < n; i++) {
-                if (roomAvailabilityTime[i] <= start) {
-                    foundUnusedRoom = true;
-                    meetingCount[i]++;
-                    roomAvailabilityTime[i] = end;
-                    break;
-                }
-
-                if (minRoomAvailabilityTime > roomAvailabilityTime[i]) {
-                    minRoomAvailabilityTime = roomAvailabilityTime[i];
-                    minAvailableTimeRoom = i;
-                }
-            }
-
-            if (!foundUnusedRoom) {
-                roomAvailabilityTime[minAvailableTimeRoom] += end - start;
-                meetingCount[minAvailableTimeRoom]++;
-            }
-        }
-        int maxMeetingCount = 0, maxMeetingCountRoom = 0;
+        auto cmpUsed = [](Meeting* a, Meeting* b) { return a->end > b->end; };
+        priority_queue<Meeting*, vector<Meeting*>, decltype(cmpUsed)> used(cmpUsed);
+        priority_queue<int, vector<int>, greater<int>> avail;
+        
+        long long time = 0;
+        int m = meetings.size(), res = 0;
+        vector<int> cnt(n, 0);
         for (int i = 0; i < n; i++) {
-            if (meetingCount[i] > maxMeetingCount) {
-                maxMeetingCount = meetingCount[i];
-                maxMeetingCountRoom = i;
+            avail.push(i);
+        }
+
+        for (int i = 0; i < m; i++) {
+            if (avail.empty()) {
+                time = max({time, used.top()->end, 0ll + meetings[i][0]});
+            } else {
+                time = max(time, 0ll + meetings[i][0]);
+            }
+            while (!used.empty() && used.top()->end <= time) {
+                avail.push(used.top()->roomId);
+                used.pop();
+            }
+            cnt[avail.top()]++;
+            used.push(new Meeting(time, time + (meetings[i][1] - meetings[i][0]), avail.top()));
+            avail.pop();
+        }
+
+        while (!used.empty()) {
+            delete used.top();
+            used.pop();
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (cnt[i] > cnt[res]) {
+                res = i;
             }
         }
-        return maxMeetingCountRoom;
+
+        return res;
     }
 };
