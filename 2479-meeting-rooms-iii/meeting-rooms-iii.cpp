f1fe48c127194c8e538ee44c00a20ea77c231cb9
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2025-07-12 00:10:31 +0530

    Time: 64 ms (90.38%) | Memory: 99.4 MB (46.06%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
@@ -1,20 +1,14 @@
-class Solution {
-private:
-    struct Meeting {
-        long long start, end;
-        int roomId;
-        Meeting(long long start, long long end, int roomId) : start(start), end(end), roomId(roomId) {}
-    };
+#define ll long long
 
+class Solution {
 public:
     Solution() { ios_base::sync_with_stdio(false); cin.tie(nullptr); }
 
     int mostBooked(int n, vector<vector<int>>& meetings) {
         sort(meetings.begin(), meetings.end());
 
-        auto cmpUsed = [](Meeting* a, Meeting* b) { return a->end > b->end; };
-        priority_queue<Meeting*, vector<Meeting*>, decltype(cmpUsed)> used(cmpUsed);
         priority_queue<int, vector<int>, greater<int>> avail;
+        priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<pair<ll, int>>> used;
         
         long long time = 0;
         int m = meetings.size(), res = 0;
@@ -25,22 +19,17 @@ public:
 
         for (int i = 0; i < m; i++) {
             if (avail.empty()) {
-                time = max({time, used.top()->end, 0ll + meetings[i][0]});
+                time = max({time, used.top().first, 0ll + meetings[i][0]});
             } else {
                 time = max(time, 0ll + meetings[i][0]);
             }
-            while (!used.empty() && used.top()->end <= time) {
-                avail.push(used.top()->roomId);
+            while (!used.empty() && used.top().first <= time) {
+                avail.push(used.top().second);
                 used.pop();
             }
             cnt[avail.top()]++;
-            used.push(new Meeting(time, time + (meetings[i][1] - meetings[i][0]), avail.top()));
+            used.push({time + (meetings[i][1] - meetings[i][0]), avail.top()});
             avail.pop();
-        }
-
-        while (!used.empty()) {
-            delete used.top();
-            used.pop();
         }
 
         for (int i = 0; i < n; i++) {
