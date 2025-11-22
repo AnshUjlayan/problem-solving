12744505bef3ad8cdfd6846c6eccd5a4df1401cd
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2025-11-22 16:21:25 +0530

    Time: 41 ms (89.65%) | Memory: 180.4 MB (78.53%) - LeetSync

--- 307-range-sum-query-mutable/range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/range-sum-query-mutable.cpp
@@ -1,59 +1,39 @@
 class NumArray {
-private:
-    int n;
-    vector<int> nums, segment;
-
-    int segmentTree(int idx, int l, int r) {
-        if(l == r) {
-            return segment[idx] = nums[l];
-        }
-        int mid = l + (r - l) / 2;
-        int left = segmentTree(idx * 2 + 1, l, mid);
-        int right = segmentTree(idx * 2 + 2, mid + 1, r);
-        return segment[idx] = left + right;
-    }
-
-    int updateSegment(int idx, int l, int r, int update) {
-        if(l > update || r < update) {
-            return segment[idx];
-        }
-        else if(l == r) {
-            return segment[idx] = nums[update];
-        }
-        int mid = l + (r - l) / 2;
-        int leftArr = updateSegment(idx * 2 + 1, l, mid, update);
-        int rightArr = updateSegment(idx * 2 + 2, mid + 1, r, update);
-        return segment[idx] = leftArr + rightArr;
-    }
-
-    int segmentSum(int idx, int l, int r, int left, int right) {
-        if(l >= left && r <= right) {
-            return segment[idx];
-        }
-        else if(l > right || r < left) {
-            return 0;
-        }
-        int mid = l + (r - l) / 2;
-        int leftArr = segmentSum(idx * 2 + 1, l, mid, left, right);
-        int rightArr = segmentSum(idx * 2 + 2, mid + 1, r, left, right);
-        return leftArr + rightArr;
-    }
-
 public:
+    vector<int> nums, fen;
+
     NumArray(vector<int>& nums) {
-        this->n = nums.size();
         this->nums = nums;
-        this->segment.resize(n * 4);
-        segmentTree(0, 0, n - 1);
+        fen.assign(nums.size() + 1, 0);
+        copy(nums.begin(), nums.end(), fen.begin() + 1);
+        for (int i = 1; i < fen.size(); i++) {
+            int parent = i + (i & -i);
+            if (parent < fen.size()) {
+                fen[parent] += fen[i];
+            }
+        }
     }
-    
+
+    int sum(int i) {
+        int res = 0;
+        while (i > 0) {
+            res += fen[i];
+            i -= i & -i;
+        }
+        return res;
+    }
+        
     void update(int index, int val) {
-        this->nums[index] = val;
-        updateSegment(0, 0, n - 1, index);
+        int delta = val - nums[index];
+        nums[index++] = val;
+        while (index < fen.size()) {
+            fen[index] += delta;
+            index += index & -index;
+        }
     }
-    
+
     int sumRange(int left, int right) {
-        return segmentSum(0, 0, n - 1, left, right);
+        return sum(right + 1) - sum(left);
     }
 };
 
