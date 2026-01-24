23cc2934906ae719cb95bdbe0fd0e56cca456bd8
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2026-01-24 21:56:05 +0530

    Time: 27 ms (60.00%) | Memory: 3.2 MB (60.00%) - LeetSync

--- 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.rs
+++ 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.rs
@@ -2,7 +2,7 @@ impl Solution {
     pub fn min_pair_sum(mut nums: Vec<i32>) -> i32 {
         let mut res = 0;
         let n = nums.len();
-        nums.sort();
+        nums.sort_unstable();
         for i in 0..(n / 2) {
             res = res.max(nums[i] + nums[n - i - 1]);
         }
