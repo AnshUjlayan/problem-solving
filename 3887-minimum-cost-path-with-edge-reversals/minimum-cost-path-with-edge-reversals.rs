3eeb3427ea05706925b9aee43bc15a5a8a103e3a
Ansh Ujlayan <86222479+AnshUjlayan@users.noreply.github.com>
2026-01-27 23:58:09 +0530

    Time: 66 ms (75.00%) | Memory: 15.8 MB (0.00%) - LeetSync

---
+++ 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.rs
@@ -0,0 +1,39 @@
+impl Solution {
+    pub fn min_cost(n: i32, edges: Vec<Vec<i32>>) -> i32 {
+        use std::cmp::Reverse;
+        use std::collections::BinaryHeap;
+
+        let n = n as usize;
+        let mut adj: Vec<Vec<(usize, i32)>> = vec![vec![]; n];
+        let mut dp = vec![1_000_000_000i32; n];
+
+        for edge in edges.iter() {
+            let u = edge[0] as usize;
+            let v = edge[1] as usize;
+            let w = edge[2];
+
+            adj[u].push((v, w));
+            adj[v].push((u, w * 2));
+        }
+
+        let mut q: BinaryHeap<(Reverse<i32>, usize)> = BinaryHeap::new();
+        q.push((Reverse(0), 0));
+
+        while let Some((Reverse(cost), node)) = q.pop() {
+            if dp[node] < cost {
+                continue;
+            } else if node == n - 1 {
+                return cost;
+            }
+
+            for &(next, w) in adj[node].iter() {
+                if dp[next] > cost + w {
+                    dp[next] = cost + w;
+                    q.push((Reverse(cost + w), next));
+                }
+            }
+        }
+
+        if dp[n - 1] == 1_000_000_000 { -1 } else { dp[n - 1] }
+    }
+}
